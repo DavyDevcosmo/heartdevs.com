88db27750f3ac31720b0244c3e6965482e31830d
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-11-01 13:11:06 -0300

    season hasmany meetings

--- app-modules/season/src/Models/Season.php
+++ app-modules/season/src/Models/Season.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace He4rt\Season\Models;
 
 use He4rt\Badge\Models\Badge;
+use He4rt\Meeting\Models\Meeting;
 use He4rt\Season\Database\Factories\SeasonFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use src\Models\Meeting;
 
 final class Season extends Model
 {
