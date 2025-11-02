2f3a61d4b36c41b84fc3bd9dce7476a426232cc4
danielhe4rt <danielhe4rt@gmail.com>
2025-11-02 17:12:54 -0300

    remove: core provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 use App\Providers\AppServiceProvider;
 use App\Providers\EventServiceProvider;
 use App\Providers\RouteServiceProvider;
-use Heart\Core\Providers\CoreProvider;
 
 return [
     AppServiceProvider::class,
     RouteServiceProvider::class,
     EventServiceProvider::class,
-    CoreProvider::class,
 ];
