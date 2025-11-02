c79f61d3309a3f5eca8b24ece997243105214b3a
danielhe4rt <danielhe4rt@gmail.com>
2025-11-02 19:17:12 -0300

    feat(tenant): add `providers` relationship and update imports

--- app-modules/tenant/database/factories/TenantFactory.php
+++ app-modules/tenant/database/factories/TenantFactory.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace He4rt\Tenant\Database\Factories;
 
-use Illuminate\Support\Facades\Date;
 use He4rt\Tenant\Models\Tenant;
 use He4rt\User\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Date;
 
 /** @extends Factory<Tenant> */
 final class TenantFactory extends Factory
--- app-modules/tenant/src/Models/Tenant.php
+++ app-modules/tenant/src/Models/Tenant.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace He4rt\Tenant\Models;
 
+use He4rt\Provider\Models\Provider;
 use He4rt\Tenant\Database\Factories\TenantFactory;
 use He4rt\User\Models\User;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model
 {
     use HasFactory;
-    use HasUuids;
+
     use SoftDeletes;
+
     protected $fillable = [
         'name',
         'slug',
@@ -27,6 +29,11 @@ class Tenant extends Model
     public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function providers(): MorphMany
+    {
+        return $this->morphMany(Provider::class, 'model');
     }
 
     protected static function newFactory(): TenantFactory
