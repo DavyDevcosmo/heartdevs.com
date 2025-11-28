af4b960b75e0939c08495a45398ef417af081772
Renan Vidal <rvidal.rvr@gmail.com>
2025-11-28 20:53:07 -0300

    feat: fix speakers card in mobile

--- app-modules/events/resources/views/components/themes/3pontos/components/sections/speakers.blade.php
+++ app-modules/events/resources/views/components/themes/3pontos/components/sections/speakers.blade.php
@@ -7,31 +7,31 @@
 
 @php
     $fodases = [
-    [
-        'avatar' => 'https://i.imgur.com/1mo38XW.jpeg',
-        'name' => 'Eduardo Vogel',
-        'role' => 'Coordenador de negócios',
-        'description' => 'Atuante no setor de tecnologia há 10 anos, formado em Administração pelo Eckerd College em St. Petersburg, Flórida, e vencedor do hackathon da Câmara dos Vereadores de São Paulo.',
-    ],
-    [
-        'avatar' => 'https://i.imgur.com/n7ZlSZ2.png',
-        'name' => 'Filipe Augusto',
-        'role' => 'CEO 3Pontos',
-        'description' => 'Empreendedor nos setores financeiro e de tecnologia, atuando na criação, expansão e gestão de negócios focados em educação financeira.',
-    ],
-    [
-        'avatar' => 'https://i.imgur.com/lxh4vKh.jpeg',
-        'name' => 'Joy Jesus',
-        'role' => 'CMO 3Pontos',
-        'description' => 'CEO e fundador da Start Digital, especializado em consultoria de marketing e posicionamento de marca.',
-    ],
-    [
-        'avatar' => 'https://i.imgur.com/UpJpsvk.jpeg',
-        'name' => 'Juliano Kimura',
-        'role' => 'CMO at Cursed Stone Game',
-        'description' => 'Palestrante e consultor em inovação digital, especialista em redes sociais, premiado no setor e experiente na gestão de equipes e projetos criativos de alto impacto.',
-    ],
-];
+        [
+            'avatar' => 'https://i.imgur.com/1mo38XW.jpeg',
+            'name' => 'Eduardo Vogel',
+            'role' => 'Coordenador de negócios',
+            'description' => 'Atuante no setor de tecnologia há 10 anos, formado em Administração pelo Eckerd College em St. Petersburg, Flórida, e vencedor do hackathon da Câmara dos Vereadores de São Paulo.',
+        ],
+        [
+            'avatar' => 'https://i.imgur.com/n7ZlSZ2.png',
+            'name' => 'Filipe Augusto',
+            'role' => 'CEO 3Pontos',
+            'description' => 'Empreendedor nos setores financeiro e de tecnologia, atuando na criação, expansão e gestão de negócios focados em educação financeira.',
+        ],
+        [
+            'avatar' => 'https://i.imgur.com/lxh4vKh.jpeg',
+            'name' => 'Joy Jesus',
+            'role' => 'CMO 3Pontos',
+            'description' => 'CEO e fundador da Start Digital, especializado em consultoria de marketing e posicionamento de marca.',
+        ],
+        [
+            'avatar' => 'https://i.imgur.com/UpJpsvk.jpeg',
+            'name' => 'Juliano Kimura',
+            'role' => 'CMO at Cursed Stone Game',
+            'description' => 'Palestrante e consultor em inovação digital, especialista em redes sociais, premiado no setor e experiente na gestão de equipes e projetos criativos de alto impacto.',
+        ],
+    ];
 
     $socials = [
         [
@@ -78,18 +78,18 @@
             @forelse ($speakers as $speaker)
                 <x-he4rt::animate-block>
                     <x-he4rt::card
-                        class="bg-elevation-01dp/32 group hover:border-b-outline-light relative h-[17rem] overflow-hidden border-b-8 transition-all duration-500 hover:gap-2"
+                        class="bg-elevation-01dp/32 group hover:border-b-outline-light relative h-[17rem] gap-2 overflow-hidden border-b-8 transition-all duration-500 hover:gap-1 lg:gap-4"
                     >
                         <x-slot:header class="border-none pb-0">
                             <div class="relative w-full transition-all duration-500 ease-in-out">
                                 <div
-                                    class="absolute top-0 transition-all duration-500 ease-in-out group-hover:left-0 group-hover:scale-75"
+                                    class="absolute top-0 scale-75 transition-all duration-500 ease-in-out group-hover:left-0 group-hover:scale-75 lg:scale-100"
                                 >
-                                    <x-he4rt::avatar size="2xl" src="{{$speaker->getFirstMediaUrl('avatar')}}" />
+                                    <x-he4rt::avatar size="2xl" src="{{ $speaker->getFirstMediaUrl('avatar') }}" />
                                 </div>
 
                                 <div
-                                    class="flex min-h-20 flex-col gap-y-2 pt-24 text-left transition-all duration-500 ease-in-out group-hover:scale-90 group-hover:justify-center group-hover:pt-0 group-hover:pl-20"
+                                    class="flex min-h-20 scale-90 flex-col justify-center gap-y-2 pt-0 pl-20 text-left transition-all duration-500 ease-in-out group-hover:scale-90 group-hover:justify-center group-hover:pt-0 group-hover:pl-20 lg:scale-100 lg:justify-start lg:pt-24 lg:pl-0"
                                 >
                                     <x-he4rt::heading level="3" size="xs">
                                         {{ $speaker->name }}
@@ -101,16 +101,21 @@
                                 </div>
                             </div>
                         </x-slot>
+
                         <div class="custom-scrollbar h-full overflow-hidden transition-all duration-500 ease-in-out">
-                            <x-he4rt::text class="line-clamp-1 transition-all duration-500 group-hover:hidden">
+                            <x-he4rt::text
+                                class="line-clamp-1 hidden transition-all duration-500 group-hover:hidden lg:block"
+                            >
                                 Ver Mais
                             </x-he4rt::text>
-                            <x-he4rt::text class="line-clamp-1 opacity-0 group-hover:opacity-100 hidden transition-all duration-500 group-hover:block group-hover:line-clamp-none">
+                            <x-he4rt::text
+                                class="opacity-100 transition-all duration-500 group-hover:line-clamp-none group-hover:opacity-100 lg:line-clamp-1 lg:opacity-0"
+                            >
                                 {{ $speaker->talks->first()->description }}
                             </x-he4rt::text>
                         </div>
 
-                        <div class="hidden gap-x-8 group-hover:flex">
+                        <div class="flex gap-x-8 group-hover:flex lg:hidden">
                             @foreach ($socials as $social)
                                 <x-he4rt::icon
                                     rel="noopener noreferrer"
@@ -127,42 +132,48 @@
             @empty
                 <p>There is no Speaker Yet.</p>
             @endforelse
-            @foreach($fodases as $fodase)
-<x-he4rt::animate-block>
+
+            @foreach ($fodases as $fodase)
+                <x-he4rt::animate-block>
                     <x-he4rt::card
-                        class="bg-elevation-01dp/32 group hover:border-b-outline-light relative h-[17rem] overflow-hidden border-b-8 transition-all duration-500 hover:gap-2"
+                        class="bg-elevation-01dp/32 group hover:border-b-outline-light relative h-[17rem] gap-2 overflow-hidden border-b-8 transition-all duration-500 hover:gap-2 lg:gap-4"
                     >
                         <x-slot:header class="border-none pb-0">
                             <div class="relative w-full transition-all duration-500 ease-in-out">
                                 <div
-                                    class="absolute top-0 transition-all duration-500 ease-in-out group-hover:left-0 group-hover:scale-75"
+                                    class="absolute top-0 scale-75 transition-all duration-500 ease-in-out group-hover:left-0 group-hover:scale-75 lg:scale-100"
                                 >
                                     <x-he4rt::avatar size="2xl" src="{{ $fodase['avatar'] }}" />
                                 </div>
 
                                 <div
-                                    class="flex min-h-20 flex-col gap-y-2 pt-24 text-left transition-all duration-500 ease-in-out group-hover:scale-90 group-hover:justify-center group-hover:pt-0 group-hover:pl-20"
+                                    class="flex min-h-20 scale-90 flex-col justify-center gap-y-2 pt-0 pl-20 text-left transition-all duration-500 ease-in-out group-hover:scale-90 group-hover:justify-center group-hover:pt-0 group-hover:pl-20 lg:scale-100 lg:justify-start lg:pt-24 lg:pl-0"
                                 >
                                     <x-he4rt::heading level="3" size="xs">
-                                        {{ $fodase['name']}}
+                                        {{ $fodase['name'] }}
                                     </x-he4rt::heading>
 
                                     <x-he4rt::text class="transition-opacity duration-300 group-hover:opacity-80">
-                                        {{ $fodase['role']}}
+                                        {{ $fodase['role'] }}
                                     </x-he4rt::text>
                                 </div>
                             </div>
                         </x-slot>
+
                         <div class="custom-scrollbar h-full overflow-hidden transition-all duration-500 ease-in-out">
-                            <x-he4rt::text class="line-clamp-1 transition-all duration-500 group-hover:hidden">
+                            <x-he4rt::text
+                                class="line-clamp-1 hidden transition-all duration-500 group-hover:hidden lg:block"
+                            >
                                 Ver Mais
                             </x-he4rt::text>
-                            <x-he4rt::text class="line-clamp-1 opacity-0 group-hover:opacity-100 transition-all duration-500 group-hover:block group-hover:line-clamp-none">
-                                {{ $fodase['description']}}    
+                            <x-he4rt::text
+                                class="opacity-100 transition-all duration-500 group-hover:line-clamp-none group-hover:opacity-100 lg:line-clamp-1 lg:opacity-0"
+                            >
+                                {{ $fodase['description'] }}
                             </x-he4rt::text>
                         </div>
 
-                        <div class="hidden gap-x-8 group-hover:flex">
+                        <div class="flex gap-x-8 group-hover:flex lg:hidden">
                             @foreach ($socials as $social)
                                 <x-he4rt::icon
                                     rel="noopener noreferrer"
@@ -177,7 +188,6 @@
                     </x-he4rt::card>
                 </x-he4rt::animate-block>
             @endforeach
-            
         </div>
     </div>
 </section>
