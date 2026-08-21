de83fe8e26e0d13f79a2d2978d3b0f3fa990ac39
danielhe4rt <danielhe4rt@gmail.com>
2026-08-21 17:05:22 -0300

    feat(marketing): module scaffold

---
+++ app-modules/marketing/src/MarketingServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace He4rt\Marketing;
+
+use Illuminate\Support\ServiceProvider;
+
+class MarketingServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
